c6bff94a098f99b10cf9bc00575e823c52bd7918
FelixB69 <fb.felixberger@gmail.com>
2022-09-12 17:32:01 +0200

    UUid use for id data base

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const typeorm = require('typeorm');
+const Wilder = require('./models/Wilder');
 
 const app = express();
 
 const dataSource = new typeorm.DataSource({
   type:"sqlite",
-  database:"wildersdb.sqlite",
+  database:"./wildersdb.sqlite",
   synchronize: true,
+  entities: [Wilder],
 });
 
 app.get('/', function (req, res) {
@@ -17,6 +19,7 @@ const PORT = 4000;
 
 async function start() {
   await dataSource.initialize();
+  await dataSource.getRepository(Wilder).save({ name: "Jean Wilder"});
   app.listen(4000, () => {
     console.log(`Server is running on port ${PORT}`)
 });
